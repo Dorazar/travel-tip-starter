1ba0c89782e25eff3133f2d0f4bd18dd892c2ae8
Dorazar <dorazar01@gmail.com>
2025-05-27 20:43:22 +0300

    Add fiter by geo location adress

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -35,11 +35,12 @@ export const locService = {
 
 function query() {
   return storageService.query(DB_KEY).then((locs) => {
-    console.log(locs)
+    
 
     if (gFilterBy.txt) {
       const regex = new RegExp(gFilterBy.txt, 'i')
-      locs = locs.filter((loc) => regex.test(loc.name))
+      locs = locs.filter((loc) => regex.test(loc.name) || regex.test(loc.geo.address))
+      console.log(locs)
     }
     if (gFilterBy.minRate) {
       locs = locs.filter((loc) => loc.rate >= gFilterBy.minRate)
